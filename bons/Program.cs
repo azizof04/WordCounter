c86778d145594a81fab554486a03c2a20602208b
Ali <azizof04@gmail.com>
2023-10-26 22:18:19 +0400

    first commit

---
+++ bons/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+  string s = " salam salam necesen qaqa";
+    List<string> fsfs = s.Split(' ').ToList();
+
+    Dictionary<string, int> dictionary = fsfs.GroupBy(x => x)
+        .ToDictionary(x => x.Key, x => x.Count());
+
+    foreach (var kvp in dictionary)
+        Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+        
